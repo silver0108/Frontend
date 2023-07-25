d3045857ede9bde45e5ec89885d68ea35b97b8ee
parkheeddong <phj7214go@gmail.com>
2023-07-25 23:12:23 +0900

    [ style ] theme.ts 추가

---
+++ src/style/theme.ts
@@ -0,0 +1,47 @@
+import { DefaultTheme } from "styled-components";
+
+const colors = {
+  // 메인컬러
+  Main: "#FBD262",
+  SUB_1: "#E63636",
+  SUB_2: "FB6262",
+};
+
+export type ColorsTypes = typeof colors;
+
+interface Font {
+  weight: number;
+  size: number;
+  lineHeight: number;
+  letterSpacing: number;
+}
+
+function FONT({ weight, size, lineHeight, letterSpacing }: Font): string {
+  return `
+    font-family: "AppleSDGothicNeo";
+    font-weight : ${weight};
+    font-size : ${size}rem;
+    line-height : ${lineHeight}rem;
+    letter-spacing : ${letterSpacing}rem;
+    `;
+}
+
+const fonts = {
+  title01: FONT({ weight: 700, size: 2.6, lineHeight: 2.6, letterSpacing: 0 }),
+  title02: FONT({ weight: 700, size: 2.0, lineHeight: 2.0, letterSpacing: 0 }),
+  title03: FONT({ weight: 500, size: 1.8, lineHeight: 2.0, letterSpacing: 0 }),
+
+  body01: FONT({ weight: 700, size: 1.6, lineHeight: 1.6, letterSpacing: 0 }),
+  body02: FONT({ weight: 500, size: 1.4, lineHeight: 2.0, letterSpacing: 0 }),
+  body03: FONT({ weight: 700, size: 1.4, lineHeight: 1.6, letterSpacing: 0 }),
+  body04: FONT({ weight: 500, size: 1.4, lineHeight: 1.6, letterSpacing: 0 }),
+  body05: FONT({ weight: 400, size: 1.2, lineHeight: 1.6, letterSpacing: 0 }),
+  body06: FONT({ weight: 500, size: 1.2, lineHeight: 1.4, letterSpacing: 0 }),
+};
+
+export type FontsTypes = typeof fonts;
+
+export const theme: DefaultTheme = {
+  colors,
+  fonts,
+};
