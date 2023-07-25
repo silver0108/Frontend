f4159c87e28378f15236c3f122fdf9434dfaeb3e
parkheeddong <phj7214go@gmail.com>
2023-07-26 00:31:48 +0900

    [ assets ] 카테고리 icon들 추가

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,2 +1,7 @@
 // index.ts
 export { ReactComponent as StarIcon } from "./icon/starIcon.svg";
+export { ReactComponent as CategoryArtIcon } from "./icon/categoryArtIcon.svg";
+export { ReactComponent as CategoryLanguageIcon } from "./icon/categoryLanguageIcon.svg";
+export { ReactComponent as CategoryExerciseIcon } from "./icon/categoryExerciseIcon.svg";
+export { ReactComponent as CategoryEtcIcon } from "./icon/categoryEtcIcon.svg";
+export { ReactComponent as CategoryMusicIcon } from "./icon/categoryMusicIcon.svg";
