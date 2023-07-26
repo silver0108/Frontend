402a38fe1a9fa950e0ed0638dfd0442329e210d1
parkheeddong <phj7214go@gmail.com>
2023-07-26 12:52:52 +0900

    [ feat ] router 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,6 +6,10 @@ import LanguagePage from "./pages/LanguagePage";
 import ArtPage from "./pages/ArtPage";
 import MusicPage from "./pages/MusicPage";
 
+import CloseDistanceTeacherPage from "./pages/CloseDistanceTeacherPage";
+import RecommendedTeacherPage from "./pages/RecommendedTeacherPage";
+import PopularTeacherPage from "./pages/PopularTeacherPage";
+
 export default function Router() {
     return (
       <BrowserRouter>
@@ -15,6 +19,9 @@ export default function Router() {
           <Route path="/music" element={<MusicPage />} />
           <Route path="/language" element={<LanguagePage />} />
           <Route path="/exercise" element={<ExercisePage />} />
+          <Route path="/close-distance" element={<CloseDistanceTeacherPage />} />
+          <Route path="/recommened-teacher" element={<RecommendedTeacherPage />} />
+          <Route path="/popular-teacher" element={<PopularTeacherPage />} />
         </Routes>
       </BrowserRouter>
     );
