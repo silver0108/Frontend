fba2df418c0947df26f854963e9d600ffb356972
parkheeddong <phj7214go@gmail.com>
2023-07-26 12:46:19 +0900

    [ feat ] router 변경

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./pages/Home";
+import ExercisePage from "./pages/ExercisePage";
+import LanguagePage from "./pages/LanguagePage";
+import ArtPage from "./pages/ArtPage";
+import MusicPage from "./pages/MusicPage";
 
 export default function Router() {
     return (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/art" element={<ArtPage />} />
+          <Route path="/music" element={<MusicPage />} />
+          <Route path="/language" element={<LanguagePage />} />
+          <Route path="/exercise" element={<ExercisePage />} />
         </Routes>
       </BrowserRouter>
     );
