79cd19d9938b78493348e33ed41126ce650c60da
parkheeddong <phj7214go@gmail.com>
2023-07-26 09:28:55 +0900

    [ feat ] category 추가

--- src/components/home/Category.tsx
+++ src/components/home/Category.tsx
@@ -1,13 +1,31 @@
+import { CategoryArtIcon, CategoryEtcIcon, CategoryExerciseIcon, CategoryLanguageIcon, CategoryMusicIcon } from "../../assets";
+
 import { styled } from 'styled-components';
-import { CategoryArtIcon, CategoryLanguageIcon, CategoryExerciseIcon, CategoryEtcIcon, CategoryMusicIcon } from "../../assets";
+
 export default function Category() {
     // icon import 오류 해결 필요
-  return (
-    <St.CategoryWrapper>
-        
-        <St.Header> 모아모아 </St.Header>
-    </St.CategoryWrapper>
-  );
+
+    function MoveToCategory(category: string) {
+      // category 로 이동하는 로직
+    }
+
+    // const categoryImgs = [CategoryLanguageIcon, CategoryExerciseIcon, CategoryArtIcon, CategoryEtcIcon];
+    // const categoryNames = ['언어', '운동', '음악', '미술', '기타'];
+
+    const categories = [['언어', CategoryLanguageIcon], ['운동', CategoryExerciseIcon], ['음악', CategoryMusicIcon], ['미술', CategoryArtIcon], ['기타', CategoryEtcIcon];
+    return (
+      <St.CategoryWrapper>
+          {categories.map((category, index) => (
+            <St.Category 
+              key = {index}
+              onClick = {() => MoveToCategory(category[0])}>
+              {category[0]}
+              {category[1]}
+            </St.Category>
+          ))}
+          <St.Header> 모아모아 </St.Header>
+      </St.CategoryWrapper>
+    );
 }
 
 const St = {
@@ -23,8 +41,7 @@ const St = {
     color: black;
   `,
   
-  StarIc: styled(StarIcon)`
-    width: 5rem;
+  Category: styled.div`
   `,
 
 
