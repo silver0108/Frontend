068a6de6bd9550a9d720d7d466eeee371c4e3a29
parkheeddong <phj7214go@gmail.com>
2023-08-01 17:16:33 +0900

    [ assets ] 하단 바 아이콘

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -9,3 +9,10 @@ export { ReactComponent as LessonImg } from "./image/lessonImg.svg";
 export { ReactComponent as TeacherImg } from "./image/teacherImg.svg";
 export { ReactComponent as BackBtnIcon } from './icon/backBtnIcon.svg';
 export { ReactComponent as DistanceIcon } from './icon/distanceIcon.svg';
+export { ReactComponent as ChattingNotActiveIcon} from './icon/chattingNotActiveIcon.svg';
+export { ReactComponent as HomeActiveIcon} from './icon/homeActiveIcon.svg';
+export { ReactComponent as ProfileNotActiveIcon} from './icon/profileNotActiveIcon.svg';
+
+export { ReactComponent as ChattingActiveIcon} from './icon/chattingActiveIcon.svg';
+export { ReactComponent as HomeNotActiveIcon} from './icon/homeNotActiveIcon.svg';
+export { ReactComponent as ProfileActiveIcon} from './icon/profileActiveIcon.svg';
