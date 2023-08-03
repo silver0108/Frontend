5f26c37d2417f8cc3d147eca3194ccad4b907169
parkheeddong <phj7214go@gmail.com>
2023-08-03 19:18:19 +0900

    [ feat ] lecture get 로직 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,13 @@
+import BottomBar from '../components/common/BottomBar';
 import Category from '../components/home/Category';
 import CloseDistanceLessonList from '../components/home/CloseDistanceLessonList';
 import Header from '../components/home/Header';
 import PopularLessonList from '../components/home/PopularLessonList';
 import PopularTeacherList from '../components/home/PopularTeacherList';
-import {styled} from 'styled-components';
-import BottomBar from '../components/common/BottomBar';
 import {WritingIcon} from '../assets';
+import {styled} from 'styled-components';
 import {useNavigate} from 'react-router-dom';
+import {useQuery} from 'react-query';
 
 export default function Home() {
   
@@ -16,6 +17,25 @@ export default function Home() {
       navigate('/writing');
     }
 
+    
+    // 전체 Lecture 정보 get
+    const { data, isLoading, error } = useQuery('lectures', fetchLectures);
+
+    async function fetchLectures() {
+        const response = await fetch('http://49.247.157.183:3000/api/lectures');
+        const data = await response.json();
+        return data;
+    }
+
+    if (isLoading) {
+        console.log('loading', data);
+    }
+
+    if (error) {
+        console.log('error', data);
+    }
+    
+   
     return (
       <St.HomeWrapper>
         <Header />
