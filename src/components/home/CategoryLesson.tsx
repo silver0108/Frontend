71e9e7e5f7a30eace0971f4e7af113e5e8c7eb30
parkheeddong <phj7214go@gmail.com>
2023-08-04 05:18:27 +0900

    [ feat ] 카테고리에 대한 레슨 컴포넌트 생성

---
+++ src/components/home/CategoryLesson.tsx
@@ -0,0 +1,125 @@
+import { CategoryArtIcon, CategoryEtcIcon, CategoryExerciseIcon, CategoryLanguageIcon, CategoryMusicIcon, DistanceIcon } from "../../assets";
+
+import { StarIcon } from '../../assets';
+import { styled } from 'styled-components';
+
+interface LessonProps {
+    key: number;
+    lessonImg : JSX.Element;
+    lessonComment : string;
+    lessonExplain : string;
+    lessonTeacher : string;
+    lessonScore : string;
+    lessonDistance : string;
+}
+
+interface LessonInfo {
+    id: number;
+    userId: number;
+    categoryId: number;
+    title: string;
+    description: string;
+    distance: number;
+    participants: number;
+    maxParticipants: number;
+    talent: string;
+    hashtags: string;
+    price: number;
+    imageUrl: null;
+    createdAt: string;
+    user : {
+        id : number;
+        username: string;
+        imageUrl: string,
+        rating: number,
+        gender: string,
+    };
+    category: {
+        id: number,
+        title: string,
+        subCategory: string,
+        imageUrl: null,
+    }
+}
+
+
+export default function CategoryLesson(props: LessonInfo) {
+
+    return (
+        <St.LessonWrapper>
+            <St.LessonImg src = {props?.user?.imageUrl}/>
+            <St.LessonExplanation> 
+                <St.Header> {props?.title} </St.Header>
+                <St.HashTags> {props?.hashtags} </St.HashTags>
+                <St.Teacher> {props?.user?.username}엄마 ⭐{props?.user?.rating} </St.Teacher> 
+                <St.Distance> 
+                    <St.DistanceIc/> 
+                    <St.DistanceNum> ~{props?.distance}m </St.DistanceNum> 
+                </St.Distance> 
+            </St.LessonExplanation>
+        </St.LessonWrapper>
+  );
+}
+
+const St = {
+    LessonWrapper: styled.section`
+        display: flex;
+        width: 32.5rem;
+        height: 6rem;
+        padding: 0.9rem 0rem 0.9rem 0rem;
+        margin-left: 1.8rem;
+        margin-bottom: 3rem;
+    `,
+
+    LessonImg : styled.img`
+        display: flex;
+        width: 6rem;
+        height: 6rem;
+        margin-right: 0.8rem;
+
+        border-radius: 20px;
+    `,
+
+    LessonExplanation: styled.div`
+        display: flex;
+        flex-direction: column;
+
+        ${({ theme }) => theme.fonts.body08};
+        color: ${({ theme }) => theme.colors.Black}; 
+    `,
+    
+    Header: styled.header`
+        ${({ theme }) => theme.fonts.body06};
+        color: ${({ theme }) => theme.colors.Black}; 
+    `,
+
+    HashTags: styled.div`
+    margin-top: 0.3rem;
+    ${({ theme }) => theme.fonts.body07};
+    color: ${({ theme }) => theme.colors.Black}; 
+    `,
+    
+    Teacher: styled.div`
+        margin-top: 0.4rem;
+        ${({ theme }) => theme.fonts.body08};
+        color: ${({ theme }) => theme.colors.Black}; 
+    `,
+
+    Distance: styled.div`
+        display: flex;
+        
+        margin-top: 0.3rem;
+    `,
+
+    DistanceIc : styled(DistanceIcon)`
+        width: 1.5rem;
+        height: 1.5rem;
+        margin-right: 0.5rem;
+        color: ${({ theme }) => theme.colors.Black}; 
+    `,
+
+    DistanceNum : styled.h3`
+        ${({ theme }) => theme.fonts.body08};
+        color: ${({ theme }) => theme.colors.Blue}; 
+    `,
+};
